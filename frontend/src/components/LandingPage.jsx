ccc079c85b95c0dc98ac67c7b22a0bbd8ccf29f9
Nirvan Timsina <somnathtimsina5@gmail.com>
2025-09-26 11:44:04 +0545

    ok finally made the sidebar work

--- frontend/src/components/LandingPage.jsx
+++ frontend/src/components/LandingPage.jsx
@@ -1,15 +1,12 @@
 "use client";
 
-
 import Product from "./Product";
 import Navbar from "./Navbar";
-
 
 export default function App() {
   return (
     <div className="bg-gray-900">
       <Navbar />
-
       <div className="relative isolate px-6 pt-14 lg:px-8">
         <div
           aria-hidden="true"
@@ -38,13 +35,11 @@ export default function App() {
               Products and Solutions That Keep The LIghts On
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">
-              A name you can trust and has been trusted in the field of consumer electronics for the last 18 years.
+              A name you can trust and has been trusted in the field of consumer
+              electronics for the last 18 years.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-              >
+              <a href="#" className="btn btn-primary">
                 Discover Prodcuts
               </a>
               <a href="#" className="text-sm/6 font-semibold text-white">
@@ -52,7 +47,6 @@ export default function App() {
               </a>
             </div>
           </div>
-          
 
           {/* this is the ending of the dark hero section thing */}
         </div>
@@ -60,15 +54,13 @@ export default function App() {
           aria-hidden="true"
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(%-30rem)]"
         >
-         
           <div
             style={{
               clipPath:
                 "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
-            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
+            className="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -z-10 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75"
           />
-          
         </div>
       </div>
     </div>
